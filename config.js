aab9ef1cdc5d4b8ac5f0b3fd1649c32acb2a9d9d
lakshya-beep <74532103+lakshya-beep@users.noreply.github.com>
2021-12-19 18:57:01 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAb95LQlBNLWAM__GgRhtWIO69nbWRi9NA",
+    authDomain: "story-telling-cf76d.firebaseapp.com",
+    databaseURL: "https://story-telling-cf76d-default-rtdb.firebaseio.com",
+    projectId: "story-telling-cf76d",
+    storageBucket: "story-telling-cf76d.appspot.com",
+    messagingSenderId: "908007996325",
+    appId: "1:908007996325:web:554055a7e887ec16ef064c"
+  };
